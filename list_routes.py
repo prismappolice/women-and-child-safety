48d6a888957617e1e09ddadf5849715d72861c3c
AP Women Safety <admin@apwomensafety.gov.in>
2025-11-25 12:58:39 +0530

    unnecessary files removed

--- list_routes.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-print("Registered Flask Routes:")
-for rule in app.url_map.iter_rules():
-    print(f"{rule.methods} {rule.rule} -> {rule.endpoint}")
--- run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    # Start the combined application
-    run_simple('localhost', 5000, application, use_reloader=True, use_debugger=True)
